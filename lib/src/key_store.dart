a6031bad739e7bec1b590fa8f63f7397a2008f3e
Mohammed Saudi <msaudi.hassan@st.uskudar.edu.tr>
2022-09-08 13:01:30 +0300

    Wrapped public and private key classes from ed library

--- lib/src/key_store.dart
+++ lib/src/key_store.dart
@@ -1,22 +1,31 @@
 import 'dart:typed_data';
 import 'package:bs58/bs58.dart';
 import 'package:ed25519_edwards/ed25519_edwards.dart' as ed;
-class KeyPair extends ed.KeyPair{
-   ed.PrivateKey privateKey;
-   ed.PublicKey publicKey;
 
-  KeyPair(this.privateKey, this.publicKey):super(privateKey, publicKey);
-
+class PrivateKey extends ed.PrivateKey {
+  PrivateKey(super.bytes);
 }
-class KeyStore {
 
-  static KeyPair newKeyPair(){
-    ed.KeyPair keypair =  ed.generateKey();
-    return  KeyPair(keypair.privateKey,keypair.publicKey);
+class PublicKey extends ed.PublicKey {
+  PublicKey(super.bytes);
+}
+
+class KeyPair extends ed.KeyPair {
+  PrivateKey privateKey;
+  PublicKey publicKey;
+
+  KeyPair(this.privateKey, this.publicKey) : super(privateKey, publicKey);
+}
+
+class KeyStore {
+  static KeyPair newKeyPair() {
+    ed.KeyPair keypair = ed.generateKey();
+    PrivateKey privateKey = PrivateKey(keypair.privateKey.bytes);
+    PublicKey publicKey = PublicKey(keypair.publicKey.bytes);
+    return KeyPair(privateKey, publicKey);
   }
 
   static String publicKeyToString(ed.PublicKey keyPair) {
-      return base58.encode(Uint8List.fromList(keyPair.bytes));
-
+    return base58.encode(Uint8List.fromList(keyPair.bytes));
   }
 }
