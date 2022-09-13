efa4830e79c5acb180967f9ed0f9cdf9725f4875
Mohammed Saudi <msaudi.hassan@st.uskudar.edu.tr>
2022-09-13 11:50:58 +0300

    fixed urls not launching in iOS devices

--- lib/src/wallet.dart
+++ lib/src/wallet.dart
@@ -3,15 +3,9 @@ import 'package:url_launcher/url_launcher.dart';
 /// redirects users to NEAR wallet for key management
 /// wrapper for connected account information i.e.
 class Wallet {
-  //WalletConnectionArgs walletConnectionParams;
   String walletURL;
-  //String approveTxURL;
-  //KeyPair keyPair;
 
-  Wallet(this.walletURL) {
-    //final launcher=injector.getService<luancher>()
-  }
-  //account, contract, appTitle,...
+  Wallet(this.walletURL);
   connect(
       contractId, appTitle, successURL, failureURL, publicKey) async {
     String url = '$walletURL&success_url=$successURL'
@@ -19,8 +13,7 @@ class Wallet {
         '&contract_id=$contractId&public_key=ed25519:'
         '$publicKey';
 
-    await launchUrl(Uri.parse(url),
-        mode: LaunchMode.externalNonBrowserApplication);
+    await launchUrl(Uri.parse(url));
   }
 
 
@@ -30,8 +23,7 @@ class Wallet {
     String url = '$walletURL&success_url=$successURL'
         '&failure_url=$failureURL&public_key=ed25519:'
         '$publicKey';
-    await launchUrl(Uri.parse(url),
-        mode: LaunchMode.externalNonBrowserApplication);
+    await launchUrl(Uri.parse(url));
   }
 
   //request transaction deposit approval from wallet
@@ -41,7 +33,6 @@ class Wallet {
         '${approveTxURL}transactions=${Uri.encodeComponent(encodedTransaction)}'
         '&callbackUrl=$successURL';
 
-    await launchUrl(Uri.parse(url),
-        mode: LaunchMode.externalNonBrowserApplication);
+    await launchUrl(Uri.parse(url));
   }
 }
