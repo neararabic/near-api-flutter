f1eda8cc940e670b516a1d5dff14a550a3b65abd
azmasamy <azmasamy173@gmail.com>
2022-10-25 17:22:48 +0300

    [UPDATE] exported utils class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.13-alpha
+Exported utils class
+
 ## 0.0.12-alpha
 Cleaned package and example
 
--- lib/near_api_flutter.dart
+++ lib/near_api_flutter.dart
@@ -6,3 +6,4 @@ export 'package:near_api_flutter/src/key_store.dart';
 export 'package:near_api_flutter/src/wallet.dart';
 export 'package:near_api_flutter/src/transaction_api/rpc_providers.dart';
 export 'package:near_api_flutter/src/models/access_key.dart';
+export 'package:near_api_flutter/src/utils.dart';
